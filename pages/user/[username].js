addcab59d2cc21715a477911847be2a0d41a6d98
evanchristians <evanryk@gmail.com>
2021-08-18 20:52:19 +0200

    added avatar

--- pages/user/[username].js
+++ pages/user/[username].js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Image from "next/image";
 import Head from "next/head";
 import useSWR from "swr";
 import { fetcher } from "../../lib/fetcher";
@@ -26,11 +27,21 @@ export default function User() {
             </Head>
 
             <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+                {user && (
+                    <div className="my-10 p-2 bg-pink-500 rounded-full text-none">
+                        <Image
+                            src={user.avatar_url}
+                            width={150}
+                            height={150}
+                            className="rounded-full"
+                        />
+                    </div>
+                )}
                 <h1 className="text-6xl font-bold">
                     {user && (
                         <>
                             User{" "}
-                            <span className="text-green-600">
+                            <span className="text-pink-500 ">
                                 {user.username}
                             </span>{" "}
                             Found!
@@ -39,7 +50,15 @@ export default function User() {
                     {error && "User Not Found!"}
                     {!user && !error && "Finding User"}
                 </h1>
-                {user && <a href={user.website} target="_blank" className="my-4 text-blue-600 underline">{user.website}</a>}
+                {user && (
+                    <a
+                        href={user.website}
+                        target="_blank"
+                        className="my-4 text-blue-600 underline"
+                    >
+                        {user.website}
+                    </a>
+                )}
                 {user && (
                     <code className="mt-5 p-3 font-mono text-lg bg-gray-100 rounded-md">
                         check browser console
