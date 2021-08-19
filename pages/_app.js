f70deff81e5e5d30c8f69103cb87e0f036c4eb5c
evanchristians <evanryk@gmail.com>
2021-08-19 23:51:33 +0200

    minor style updates

--- pages/_app.js
+++ pages/_app.js
@@ -5,19 +5,7 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head></Head>
       <Component {...pageProps} />
     </>
   );
--- pages/user/[username]/[mode].jsx
+++ pages/user/[username]/[mode].jsx
@@ -25,27 +25,25 @@ export default function User() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>User {username}</title>
+        <title>
+          {username} | {mode}
+        </title>
       </Head>
 
-      <main className="flex flex-col items-start w-full flex-1 px-20 py-10 text-center">
-        {user && (
-          <div className="mb-5">
-            <Image
-              src={user.avatar_url}
-              width={75}
-              height={75}
-              className="rounded-full"
-            />
-          </div>
-        )}
-        <InView>
-          {({ inView, ref }) => (
-            <h1
-              data-scroll
-              ref={ref}
-              className={`text-4xl font-bold ${inView && "in-view"}`}
-            >
+      <main className="flex flex-col items-start w-full flex-1 px-20 py-10">
+        <div className="flex items-center gap-5 mb-10">
+          {user && (
+            <div>
+              <Image
+                src={user.avatar_url}
+                width={80}
+                height={80}
+                className="rounded-full"
+              />
+            </div>
+          )}
+          <div>
+            <h1 className="text-4xl font-bold">
               {user && (
                 <>
                   <span className="text-pink-500 ">{user.username}</span>
@@ -54,21 +52,21 @@ export default function User() {
               {error && "User Not Found!"}
               {!user && !error && "Finding User"}
             </h1>
-          )}
-        </InView>
-        {user && (
-          <p className="font-bold text-xl text-yellow-500">{user.playmode}</p>
-        )}
-        {user && (
-          <p className="mb-10">
-            {user.country.name} ({user.country.code})
-          </p>
-        )}
-        {user && user.is_supporter ? (
-          <p className="px-2 py-1 rounded-md bg-yellow-400 text-white text-lg my-2">
-            supporter
-          </p>
-        ) : null}
+            {user && (
+              <p className="font-bold text-xl text-yellow-500">{mode}</p>
+            )}
+            {user && (
+              <p>
+                {user.country.name} ({user.country.code})
+              </p>
+            )}
+          </div>
+          {user && user.is_supporter ? (
+            <p className="px-2 py-1 rounded-md bg-yellow-400 text-white text-lg my-2">
+              supporter
+            </p>
+          ) : null}
+        </div>
         <div className="flex w-full gap-5">
           {user && <StatBlock title="PP" score={user.statistics.pp} />}
           {user && (
